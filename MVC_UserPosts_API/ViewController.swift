8354424393d9c574a39d0fa98d361c2010e867f4
SureshSindam <sindam.suresh@gmail.com>
2023-12-28 15:06:20 -0600

    Initial Commit

---
+++ MVC_UserPosts_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_UserPosts_API
+//
+//  Created by Suresh Sindam on 12/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
